757171c4c8ac1bc49622c21a984baaed5dc56055
vs-machado <vinicius.s.machado@protonmail.com>
2023-11-09 13:29:58 -0300

    Project creation

---
+++ app/src/main/java/com/phoenix/pillreminder/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.phoenix.pillreminder.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.phoenix.pillreminder.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

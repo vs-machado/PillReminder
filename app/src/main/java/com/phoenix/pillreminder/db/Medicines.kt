741043e7166b0d10eab58e793c61c1e3f4e66fb3
vs-machado <vinicius.s.machado@protonmail.com>
2023-12-08 22:48:38 -0300

    Medicines database created

--- app/src/main/java/com/phoenix/pillreminder/db/Medicines.kt
+++ app/src/main/java/com/phoenix/pillreminder/db/Medicines.kt
@@ -1,3 +1,19 @@
 package com.phoenix.pillreminder.db
 
-data class Medicines()
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "medicines_data_table")
+data class Medicines(
+
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "medicine_id")
+    var id:Int,
+    @ColumnInfo(name = "medicine_name")
+    var name:String,
+    @ColumnInfo(name = "medicine_strength")
+    var strength:String,
+    @ColumnInfo(name = "medicine_quantity")
+    var quantity:Int
+)

520838eb831e8216973ed98686dc962dd3c4c610
vs-machado <vinicius.s.machado@protonmail.com>
2024-03-14 12:50:13 -0300

    Horizontal datepicker from HomeFragment word Today is not hardcoded anymore (it shows according to user's system language)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -77,9 +77,6 @@ dependencies {
     //Gson
     implementation("com.google.code.gson:gson:2.10.1")
 
-    //Horizontal Date Picker
-    implementation("com.github.myDario:DarioWeekViewDatePicker:1.0.3")
-
     implementation("androidx.core:core-ktx:$coreVersion")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")

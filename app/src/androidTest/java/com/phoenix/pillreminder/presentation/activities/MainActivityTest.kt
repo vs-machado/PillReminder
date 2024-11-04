77a8ddfa75e81501aa7a3ab570c2264a7e951ee6
vs-machado <vinicius.s.machado@protonmail.com>
2024-11-04 13:10:30 -0300

    Moved languageMapping to a separate file

--- app/src/androidTest/java/com/phoenix/pillreminder/presentation/activities/MainActivityTest.kt
+++ app/src/androidTest/java/com/phoenix/pillreminder/presentation/activities/MainActivityTest.kt
@@ -16,6 +16,7 @@ import org.junit.runner.RunWith
 import java.util.Locale
 import javax.inject.Inject
 import com.google.common.truth.Truth.assertThat
+import com.phoenix.pillreminder.feature_alarms.presentation.utils.languageMapping
 
 // Language tests needs to be run in a phone using English language as default
 @HiltAndroidTest
@@ -28,7 +29,6 @@ class MainActivityTest {
     @get:Rule
     val instantTaskExecutorRule = InstantTaskExecutorRule()
 
-    private lateinit var languageMapping: Map<String, String>
 
     @Inject
     lateinit var sharedPreferencesRepository: SharedPreferencesRepository
@@ -36,25 +36,6 @@ class MainActivityTest {
     @Before
     fun setUp() {
         hiltRule.inject()
-        languageMapping = mapOf(
-            // English regions
-            "en-US" to "en", "en-GB" to "en", "en-AU" to "en",
-            "en-CA" to "en", "en-IN" to "en", "en-NZ" to "en",
-            "en-ZA" to "en", "en-IE" to "en", "en-JM" to "en",
-            "en" to "en",
-            // Portuguese region
-            "pt-BR" to "pt-BR",
-            // Spanish regions
-            "es-ES" to "es", "es-MX" to "es", "es-AR" to "es",
-            "es-CO" to "es", "es-CL" to "es", "es-PE" to "es",
-            "es-VE" to "es", "es-EC" to "es", "es-GT" to "es",
-            "es-CU" to "es", "es-BO" to "es", "es-DO" to "es",
-            "es-HN" to "es", "es-PY" to "es", "es-SV" to "es",
-            "es-NI" to "es", "es-CR" to "es", "es-PR" to "es",
-            "es-PA" to "es", "es-UY" to "es", "es" to "es",
-            // Italian regions
-            "it-IT" to "it", "it-CH" to "it", "it-SM" to "it", "it" to "it"
-        )
     }
 
     @Test
